ebb23d22ab5185b45a752b8ea20cc4fa7ee5bc12
Ayushmaan <ayushmaan703@gmail.com>
2025-08-09 18:26:18 +0530

    changes made in slient side server side errors

--- app.js
+++ app.js
@@ -1,117 +1,141 @@
 const express = require("express");
-const app= express();
+const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride=require("method-override");
-const ejsMate= require("ejs-mate");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { wrap } = require("module");
 
-
-main().then(() => {
+main()
+  .then(() => {
     console.log("connected to db");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
-    
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
-
-app.get("/", (req,res) =>{
-    res.send("hi its root");
+app.get("/", (req, res) => {
+  res.send("hi its root");
 });
 
 //index route
-app.get("/listings", wrapAsync( async (req,res) => {
-  const allListings=  await Listing.find({});
-  res.render("listings/index.ejs",{allListings});
-})); 
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 
 //new route
-app.get("/listings/new", (req,res) =>{
-    res.render("listings/new.ejs");
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
- 
+
 //show route
-app.get("/listings/:id",  wrapAsync(async (req, res) => {
-    let {id}= req.params;
-   const listing = await Listing.findById(id);
-   res.render("listings/show.ejs", { listing });
-}));
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
 
 //create route
-app.post("/listings", wrapAsync(async (req,res,next) => {
-    if(!req.body.listing) {
-        throw new ExpressError(400, "send valid data for listing");
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "send valid data for listing");
     }
-    
- const newListing= new Listing(req.body.listing);
+
+    const newListing = new Listing(req.body.listing);
+    if (!newListing.title) {
+      throw new ExpressError(400, "title is missing");
+    }
+    if (!newListing.description) {
+      throw new ExpressError(400, "description is missing");
+    }
+    if (!newListing.location) {
+      throw new ExpressError(400, "location is missing");
+    }
+
     await newListing.save();
     res.redirect("/listings");
-
-   })
+  })
 );
 
 //edit route
-app.get("/listings/:id/edit", wrapAsync( async (req,res) => {
-     let {id}= req.params;
-   const listing = await Listing.findById(id);
-   res.render("listings/edit.ejs",{listing});
-}));
-
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
 
 //update route
-app.put("/listings/:id", wrapAsync( async (req,res) => {
-     if(!req.body.listing) {
-        throw new ExpressError(400, "send valid data for listing");
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new ExpressError(400, "send valid data for listing");
     }
-    let {id}= req.params;
-     await Listing.findByIdAndUpdate(id,{...req.body.listing});
-     res.redirect(`/listings/${id}`);
-}));
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //delete route
-app.delete("/listings/:id", wrapAsync( async (req,res) => {
-    let {id} = req.params;
-    id=id.trim();
-    let deletedListing= await Listing.findByIdAndDelete(id);
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    id = id.trim();
+    let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-}));
+  })
+);
 // app.get("/testListing", async (req,res) => {
-    //  let sampleListing= new Listing({
-    //     title:"my new villa",
-    //     description:"by the beach",
-    //     price:1200,
-    //     location:"calungate goa",
-    //     country:"india",
-    // });
-    // await sampleListing.save();
-    // console.log("sample was saved");
-    // res.send("successfull testing");
+//  let sampleListing= new Listing({
+//     title:"my new villa",
+//     description:"by the beach",
+//     price:1200,
+//     location:"calungate goa",
+//     country:"india",
+// });
+// await sampleListing.save();
+// console.log("sample was saved");
+// res.send("successfull testing");
 // });
 
-app.all("*", (req,res,next) =>{
-    next(new ExpressError(404,"page not found"));
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "page not found"));
 });
 
-app.use((err,req,res,next) => {
-    let { statusCode=500, message= "something went wrong!" } = err;
-    res.status(statusCode).send(message);
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  //res.status(statusCode).send(message);
 });
 
-app. listen(8080, () => {
-    console.log("sever is listening");
+app.listen(8080, () => {
+  console.log("sever is listening");
 });
--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,5 +1,7 @@
-module.exports=(fn) => { 
-    return (req,res,next) => {
-        fn(req,res,next).catch(next);
-    }
-}
+// utils/wrapAsync.js
+module.exports = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);s
+  };
+};
+ 
